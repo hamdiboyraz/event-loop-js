f24e32207b9daa45cc1edb1be9bf7d53b4e1d74c
hamdiboyraz <hamdiboyraz19@gmail.com>
2022-12-17 00:43:05 +0300

    Thread pool

--- event-loop.js
+++ event-loop.js
@@ -1,4 +1,9 @@
 const fs = require("fs");
+const crypto = require("crypto");
+
+const start = Date.now();
+//process.env.UV_THREADPOOL_SIZE = 1; This code is not working on Windows
+// In order to run this way, change ->set UV_THREADPOOL_SIZE=1 & node event-loop.js
 
 setTimeout(() => console.log("Timer 1 finished"), 0);
 setImmediate(() => console.log("Immediate 1 finished"));
@@ -12,6 +17,22 @@ fs.readFile("test-file.txt", () => {
   setImmediate(() => console.log("Immediate 2 finished"));
 
   process.nextTick(() => console.log("Process.nextTick"));
+
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
+  crypto.pbkdf2("password", "salt", 100000, 1024, "sha512", () => {
+    console.log(Date.now() - start, "Password encrypted");
+  });
 });
 
 console.log("Hello from the top-level code");
